2b12d87b6b639bb49341ba118e792b7b4e1cb387
LTurret <a0919329@gmail.com>
2023-12-10 21:20:58 +0800

    general code improvement

--- embed_generator.py
+++ embed_generator.py
@@ -1,7 +1,15 @@
 from interactions import Embed
 
 
-def embed_generator(content: dict, media: str | None = None, tweetId: str | None = None, color: hex = 0x1DA0F2) -> Embed:
+def embed_generator(
+    content: dict,
+    media: str | None = None,
+    tweetId: str | None = None,
+    color: hex = 0x1DA0F2,
+    footer_text: str = "樓梯的推特連結修復魔法",
+    icon_url: str = "https://abs.twimg.com/icons/apple-touch-icon-192x192.png",
+    minimal: bool = False,
+) -> Embed:
     embed: Embed = Embed(description=content["full_text"], color=color, timestamp=content["created_timestamp"], url="https://twitter.com")
     embed.set_author(
         name=f"{content['author']} (@{content['screen_name']})",
@@ -9,11 +17,14 @@ def embed_generator(content: dict, media: str | None = None, tweetId: str | None
         icon_url=content["icon_url"],
     )
     embed.set_footer(
-        text="樓梯的推特連結修復魔法",
-        icon_url="https://abs.twimg.com/icons/apple-touch-icon-192x192.png",
+        text=footer_text,
+        icon_url=icon_url,
     )
-    embed.add_field(name="愛心數", value=f'{int(content["favorite_count"]):3,d}', inline=True)
-    embed.add_field(name="轉推數", value=f'{int(content["retweet_count"]):3,d}', inline=True)
+
+    if not minimal:
+        embed.add_field(name="愛心數", value=f'{int(content["favorite_count"]):3,d}', inline=True)
+        embed.add_field(name="轉推數", value=f'{int(content["retweet_count"]):3,d}', inline=True)
+    
     embed.add_field(name="推文傳送門", value=f"[點我！](https://fxtwitter.com/i/status/{tweetId})", inline=True)
 
     if media:
